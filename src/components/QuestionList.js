6ea248deb961b1bd232755f9cb70bdc57de7f302
dev3bd <9abdalrahmn9@gmail.com>
2024-05-22 20:44:29 +0300

    It's hard

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data));
+  }, []);
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      {questions.map(question => (
+        <div key={question.id}>
+          <h3>{question.prompt}</h3>
+          <ul>
+            {question.answers.map((answer, index) => (
+              <li key={index}>{answer}</li>
+            ))}
+          </ul>
+          <p>Correct Index: {question.correctIndex}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
